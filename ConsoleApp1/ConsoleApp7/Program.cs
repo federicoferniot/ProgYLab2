a7342cedbaf7dd0dfee59d0f22dd29972a3d2cc9
fferniot <feder.fer.93@gmail.com>
2018-08-27 18:42:07 -0300

    primeros ejercicios

---
+++ ConsoleApp1/ConsoleApp7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 07";
+            double cantidadDias;
+            int año=0;
+            int mes=0;
+            int dia=0;
+            string añoStr="";
+            string diaStr="";
+            string mesStr="";
+
+            while (!int.TryParse(añoStr, out año) || año<0)
+            {
+                Console.WriteLine("Ingrese el año de su fecha de nacimiento");
+                añoStr = Console.ReadLine();
+            }
+
+            while (!int.TryParse(mesStr, out mes) || (mes<0 || mes>12))
+            {
+                Console.WriteLine("Ingrese el mes");
+                mesStr = Console.ReadLine();
+            }
+
+            while (!int.TryParse(diaStr, out dia) || (dia<0 || dia>31))
+            {
+                Console.WriteLine("Ingrese el día");
+                diaStr = Console.ReadLine();
+            }
+
+            DateTime fechaDeNacimiento = new DateTime(año, mes, dia);
+            DateTime fechaActual = DateTime.Now;
+            cantidadDias = (fechaActual - fechaDeNacimiento).TotalDays;
+            Console.WriteLine("Cantidad de dias vividos: {0}", cantidadDias);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Ejercicio_09
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 09";
+            int nivelPiramide;
+            string caracterADibujar;
+            string nivelPiramideStr="";
+
+            while (!int.TryParse(nivelPiramideStr, out nivelPiramide) || nivelPiramide < 0)
+            {
+                Console.WriteLine("Ingrese el nivel de la piramide");
+                nivelPiramideStr = Console.ReadLine();
+            }
+
+            for (int i = 0, cantidadEnNivel = 1; i < nivelPiramide; i++,cantidadEnNivel+=2)
+            {
+                caracterADibujar = new String('*', cantidadEnNivel);
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (caracterADibujar.Length / 2)) + "}", caracterADibujar));
+            }
+            Console.ReadKey();
+        }
+    }
+}
