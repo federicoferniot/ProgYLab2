62accdb4b6faa5bb26ac8d11540cf7ca0c099f88
alumno <alumno>
2018-10-01 22:26:02 -0300

    Ejercicio clase 10

---
+++ Clase 10/Clase_9_Library/Accionista.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_Library
+{
+    public class Accionista: Persona
+    {
+        private int porcionAccionaria;
+
+        public int PorcionAccionaria
+        {
+            get { return porcionAccionaria; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    this.porcionAccionaria = value;
+                }
+            }
+        }
+
+        public Accionista(string nombre, string apellido, int porcionAccionaria): base(nombre, apellido)
+        {
+            this.PorcionAccionaria = porcionAccionaria;
+        }
+
+        public override string PosicionSocietaria()
+        {
+            return String.Format("Accionista con el {0}% de la porción accionaria", this.PorcionAccionaria);
+        }
+
+        public static bool operator ==(Accionista accionistaUno, Accionista accionistaDos)
+        {
+            return (accionistaUno.PorcionAccionaria == accionistaDos.PorcionAccionaria);
+        }
+
+        public static bool operator !=(Accionista accionistaUno, Accionista accionistaDos)
+        {
+            return !(accionistaUno == accionistaDos);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(base.Mostrar());
+            sb.AppendLine(this.PosicionSocietaria());
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Clase 10/Clase_9_Library/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_Library
+{
+    public abstract class Persona
+    {
+        protected string _nombre;
+        protected string _apellido;
+
+        public Persona(string nombre, string apellido)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+        }
+        public string Apellido
+        {
+            get
+            {
+                return this._apellido;
+            }
+        }
+
+        public abstract string PosicionSocietaria();
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Nombre  : " + this._nombre);
+            sb.AppendLine("Apellido: " + this._apellido);
+            return sb.ToString();
+        }
+    }
+}
