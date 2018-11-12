55cefd84a9c8c05c0b531a48168b7741a60ee64d
alumno <alumno>
2018-11-12 20:21:14 -0300

    Ejercicio clase delegado

---
+++ EjercicioClase/EjercicioClase/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioClase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormLlenar form = new FormLlenar();
+            form.Show(this);
+            form.EnviarMensaje += this.NoHaceNada;
+            foreach(Form x in this.OwnedForms)
+            {
+                if(x is Form2)
+                {
+
+                    form.EnviarMensaje += ((Form2)x).ManejadorEvento;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            foreach(Form x in this.OwnedForms)
+            {
+                if(x is FormLlenar)
+                {
+                    ((FormLlenar)x).EnviarMensaje += form2.ManejadorEvento;
+                }
+            }
+            form2.Show(this);
+        }
+
+        public void NoHaceNada(string nada) { }
+    }
+}
---
+++ EjercicioClase/EjercicioClase/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioClase
+{
+    public partial class Form2 : Form
+    {
+        public delegate void Evento(string mensaje);
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void ManejadorEvento(string mensaje)
+        {
+            this.textBox1.Text = textBox1.Text + mensaje;
+        }
+
+    }
+}
